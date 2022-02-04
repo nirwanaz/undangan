d4bcad62dba3aaf210e3fd3d4ebd681828a17e2f
nirwanaz <90511965+nirwanaz@users.noreply.github.com>
2022-02-04 17:56:12 +0700

    landing page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -12,7 +12,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<style type="text/css">
 		section {
 			margin-top: 50px;
-			margin-bottom: 50px;
 		}
 	</style>
 </head>
@@ -26,14 +25,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="collapse navbar-collapse" id="navbarSupportedContent">
 				<ul class="navbar-nav mx-auto mb-2 mb-lg-0">
 					<li class="nav-item">
-						<a class="nav-link active" aria-current="page" href="#">Home</a>
+						<a class="nav-link active" aria-current="page" href="#">Beranda</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="#">Product</a>
+						<a class="nav-link" href="#">Fitur</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-							Article
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown-menu" aria-expanded="false">
+							Tema
 						</a>
 						<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
 							<li><a class="dropdown-item" href="#">Action</a></li>
@@ -43,12 +42,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 						</ul>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+						<a class="nav-link" href="#">Kontak</a>
 					</li>
 				</ul>
 				<form class="d-flex">
-					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
+					<input class="form-control me-2" type="search" placeholder="Pencarian" aria-label="Search">
+					<button class="btn btn-outline-success" type="submit">Cari</button>
 				</form>
 			</div>
 		</div>
@@ -82,9 +81,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="col-md">
 						<h1 style="text-transform: capitalize;" class="text-center">contoh template undangan pernikahan online</h1>
 						<div class="row">
-							<?php foreach(range(0,5) as $number) { ?>
-							<div class="col-md">
-								<div class="card">
+							<?php foreach(range(1,5) as $number) { ?>
+							<div class="col-md-2">
+								<div class="card my-2">
 									<img 
 										src="https://cdn.pixabay.com/photo/2022/01/02/22/06/couple-6911271_960_720.png" 
 										alt="card-img"
@@ -97,6 +96,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 								</div>
 							</div>
 							<?php } ?>
+							<div class="col-md-2">
+								<div class="card my-2">
+									<img 
+										src="https://cdn.pixabay.com/photo/2015/11/03/08/56/question-mark-1019820_960_720.jpg" 
+										alt="card-img"
+										class="card-img-top" >
+									
+									<div class="card-body">
+										<h5 class="card-title">custom</h5>
+										<a href="#" class="btn btn-primary w-100">See</a>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -108,15 +120,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					<div class="col-md">
 						<h1 style="text-transform: capitalize;" class="text-center">fitur undangan pernikahan online</h1>
 						<div class="row">
-							<?php foreach(range(0,5) as $number) {?>
-							<div class="col-md-4">
+							<?php foreach(['countdown', 'guest-list', 'map', 'music', 'photo-album', 'schedule', 'video-player'] as $feature) {?>
+							<div class="col-sm-6 col-lg-3">
 								<div class="card my-2">
 									<img 
-										src="https://cdn-icons-png.flaticon.com/512/498/498919.png" 
+										src="<?php echo './assets/icons/feature/'.$feature.'.png' ?>" 
 										alt="card-img-features"
-										class="card-img-top">
+										class="card-img-top p-2"
+										>
 									<div class="card-body">
-										<h5 class="card-title"><?php echo 'Fetaure'.$number ?></h5>
+										<h5 class="card-title"><?php echo $feature ?></h5>
 										<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel libero sit amet metus semper tristique in ut ipsum.</p>
 									</div>
 								</div>
@@ -127,7 +140,48 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 				</div>
 			</div>
 		</section>
+		<section class="section-price">
+			<div class="container">
+				<div class="row">
+					<div class="col-md bg-warning p-3 rounded-3">
+						<div class="row justify-content-center">
+							<div class="col-md-6 text-center">
+								<h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel libero sit amet metus semper tristique in ut ipsum.</h5>
+								<button class="btn btn-primary" type="button">Dapatkan Sekarang</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
+	<div class="footer bg-info mt-4">
+		<div class="container py-5">
+			<div class="row">
+				<div class="col-md-6 p-3">
+					<div class="heading">
+						<h3>iniundanganmu</h3>
+					</div>
+					<div class="mt-3">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel libero sit amet metus semper tristique in ut ipsum.</p>
+					</div>
+				</div>
+				<div class="col-md-6 p-3">
+					<div class="heading">
+						<h3>Kontak Kami</h3>
+					</div>
+					<div class="mt-3">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vel libero sit amet metus semper tristique in ut ipsum.</p>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+	<!-- Popper JS and Bootstrap JS -->
+	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
 
 </body>
 </html>
